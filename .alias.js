33b1ac256667f4c6aeefbed457086750c7d5eb37
Ricardo Fernández Serrata <76864299+Rudxain@users.noreply.github.com>
2022-10-04 16:54:03 -0400

    Update .alias.js

--- .alias.js
+++ .alias.js
@@ -1,5 +1,8 @@
 /*
-These are for Node and browser-console.
-intended to be copy-pasted.
+these are intended to be copy-pasted.
+`abs` & `sign` are generic, to support `BigInt`s
 */
-const {log: print, clear: cls} = console
+const
+	{log: print, clear: cls} = console,
+	abs = x => x < 0 ? -x : x,
+	sign = x => x == 0 ? x : x / abs(x)
