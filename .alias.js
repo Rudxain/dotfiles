0994607c98973665be30c563f7914fdb811dc61d
Ricardo Fernández Serrata <76864299+Rudxain@users.noreply.github.com>
2023-03-09 18:48:02 -0400

    Delete .alias.js

--- .alias.js
+++
@@ -1,8 +0,0 @@
-/*
-these are intended to be copy-pasted.
-`abs` & `sign` are generic, to support `BigInt`s
-*/
-const
-	{log: print, clear: cls} = console,
-	abs = x => x < 0 ? -x : x,
-	sign = x => x == 0 ? x : x / abs(x)
