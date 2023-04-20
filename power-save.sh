ed41bfcfc9192f26f2122bde77c6783f3f6c7911
Ricardo Fernández Serrata <76864299+Rudxain@users.noreply.github.com>
2023-04-20 14:41:40 -0400

    Update power-save.sh

--- power-save.sh
+++ power-save.sh
@@ -11,7 +11,8 @@ shopt -s expand_aliases
 DEFAULT_DPI=480
 
 alias adbsh='adb shell'
-alias sett='adbsh settings put global'
+alias setg='adbsh settings put global'
+alias sety='adbsh settings put system'
 
 power_save_on="$(adbsh settings get global low_power)"
 if [[ "$power_save_on" == 1 ]]
@@ -27,17 +28,18 @@ else
 	dpi="$(( DEFAULT_DPI / 2 ))"
 fi
 
-sett low_power "$power_save_off"
+setg low_power "$power_save_off"
 
 for s in animator_duration_scale transition_animation_scale window_animation_scale
 do
-	sett "$s" "$half"
+	setg "$s" "$half"
 done
 
 adbsh wm size "$resolution"
 adbsh wm density "$dpi"
 
-adbsh settings put system double_tab_to_wake_up "$power_save_on"
+sety double_tab_to_wake_up "$power_save_on"
+sety charging_info_always "$power_save_on"
 
 # commented-out, because I always have it enabled
-#adbsh settings put system accessibility_reduce_transparency "$power_save_off"
+#sety accessibility_reduce_transparency "$power_save_off"
