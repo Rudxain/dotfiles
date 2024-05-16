e1e02149188cad89247f67aa3239487744f85d5f
Ricardo Fernández Serrata <76864299+Rudxain@users.noreply.github.com>
2024-05-16 15:59:54 -0400

    avoid assigning to `PREFIX` in `etc_path.sh`

--- etc_path.sh
+++ etc_path.sh
@@ -5,7 +5,7 @@ if [ -d /system/bin ]; then
 	PATH="$PATH:/system/bin"
 fi
 
-if [ -d "${PREFIX=/usr}/local/bin" ]; then
+if [ -d "${PREFIX:-/usr}/local/bin" ]; then
 	PATH="$PATH:${PREFIX=/usr}/local/bin"
 fi
 
